3a2124136be177252f5f194fa92ec87b18603c2c
Dev.Jin <nara04040@naver.com>
2022-07-03 14:12:17 +0900

    add visual but must fix header gnb

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    menuList = $('.menu-list');
+    header = $('.header')
+    subMenu = $('.sub-menu')
+    headerShow = $('.header-show');
+    subMenuShow = $('.sub-menu-show')
+    // menuList.mouseenter(function () {
+    //     // header.addClass('header-show');
+    //     subMenu.addClass('sub-menu-show');
+    // });
+    // menuList.mouseleave(function () {
+    //     header.removeClass('header-show');
+    //     subMenu.removeClass('sub-menu-show');
+    // });
+    $('.menu-list').mouseenter(function(){
+        $('.header').addClass('header-show');
+        $('.sub-menu-list').addClass('sub-menu-show');
+    })
+    $('.menu-list').mouseleave(function(){
+        $('.header').removeClass('header-show');
+        $('.sub-menu-list').removeClass('sub-menu-show')
+    })
+
+    // swiper
+    new Swiper('.sw-container',{ 
+        loop: true
+    })
+})
