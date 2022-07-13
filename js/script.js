dc518a2148a10d326b39a9fc184ed90b0974567d
Dev.Jin <nara04040@naver.com>
2022-07-13 09:22:43 +0900

    html,css,js 대폭수정중..

--- js/script.js
+++ js/script.js
@@ -1,45 +1,43 @@
-$(document).ready(function() {
-    menuList = $('.menu-list');
-    header = $('.header')
-    subMenu = $('.sub-menu')
-    headerShow = $('.header-show');
-    subMenuShow = $('.sub-menu-show')
-    // menuList.mouseenter(function () {
-    //     // header.addClass('header-show');
-    //     subMenu.addClass('sub-menu-show');
-    // });
-    // menuList.mouseleave(function () {
-    //     header.removeClass('header-show');
-    //     subMenu.removeClass('sub-menu-show');
-    // });
-    $('.menu-list').mouseenter(function(){
-        $('.header').addClass('header-show');
-        $('.sub-menu-list').addClass('sub-menu-show');
+$(document).ready(function () {
+    // header
+    let header = $('.header');
+    let gnb = $('.gnb');
+    let depth_1 = $('.depth1');
+    let depth_1_List = $('.depth1 > li')
+    let depthUnderLine = $('.depth1 > li::after')
+    let depth_2 = $('.depth2');
+    let gnbMaxHeight = gnb.outerHeight();
+    let gnbMinHeight = header.outerHeight();
+
+    depth_1.mouseenter(function () {
+        header.css('height', gnbMaxHeight);
+
     })
-    $('.menu-list').mouseleave(function(){
-        $('.header').removeClass('header-show');
-        $('.sub-menu-list').removeClass('sub-menu-show')
+    depth_1.mouseleave(function () {
+        header.css('height', gnbMinHeight);
+    });
+    //visual
+    let swiperVisual = new Swiper('.sw-visual', {
+        loop:true,
+        navigation : {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
     })
 
-    // swiper
-    new Swiper('.sw-container',{ 
-        loop: true,
-        navigation: {
-            nextEl:'.swiper-button-next',
-            prevEl:'.swiper-button-prev',
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            type: 'fraction',
-        },
-    })
+
+})
+
+
+
+window.onload = function () {
     // 마우스 따라다니기
     const arrowBtn = document.querySelector('.swiper-button-next');
 
-    document.addEventListener('mousemove', (e) =>{
+    document.addEventListener('mousemove', (e) => {
         const mouseX = e.clientX;
         const mouseY = e.clientY;
         arrowBtn.style.left = mouseX + 'px';
         arrowBtn.style.top = mouseY + 'px';
     })
-})
+}
